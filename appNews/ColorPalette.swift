0e0ff17080736c15b89d7741853e5ec0edeb9249
George Michelon <michelon.george@gmail.com>
2017-04-10 13:50:38 -0300

    testandot

---
+++ appNews/ColorPalette.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPalete.swift
+//  appNews
+//
+//  Created by Developer-01 on 09/04/17.
+//  Copyright © 2017 George Michelon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+enum ColorPalette {
+    
+    static let red =  UIColor(red:0.910, green: 0.282, blue: 0.333, alpha: 1.000)
+    
+    static let white =  UIColor(red:1.000, green: 0.988, blue: 0.976, alpha: 1.000)
+    
+    static let black =  UIColor(red:0.251, green: 0.247, blue: 0.298, alpha: 1.000)
+}
---
+++ appNews/Uimage+KingFish.swift
@@ -0,0 +1,9 @@
+//
+//  Uimage+KingFish.swift
+//  appNews
+//
+//  Created by Developer-01 on 09/04/17.
+//  Copyright © 2017 George Michelon. All rights reserved.
+//
+
+import Foundation
